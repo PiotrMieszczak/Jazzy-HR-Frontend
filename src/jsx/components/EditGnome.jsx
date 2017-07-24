67bf2f0f2348a0621a2219276cb07790133c0168
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-24 17:09:57 +0200

    created form

--- src/jsx/components/EditGnome.jsx
+++ src/jsx/components/EditGnome.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
-class GnomeList extends React.Component{
+class EditGnome extends React.Component{
 
+    render(){
+        return <form>
+            <label key={'gnomeNameLabel'} htmlFor="name">Gnome name:</label>
+            <input 
+            key={'gnomeName'} type="text" name="name"/>
+            <label key={'gnomeAgeLabel'} htmlFor="age">Gnome name:</label>
+            <input 
+            key={'gnomeAge'} type="text" name="age"/>
+            <label key={'gnomeStrLabel'} htmlFor="str">Strenght:</label>
+            <input 
+            key={'gnomeStr'} type="text" name="str"/>
+            <input 
+            type="submit" value="SAVE"/>
+        </form>    
+    }
 
 }
 
-module.exports = GnomeList;
+module.exports = EditGnome;
