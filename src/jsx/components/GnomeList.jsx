006078b2db44705c47f1ebbc19d93839253d3ac7
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-24 14:41:00 +0200

    added loadData fn to fetch data

--- src/jsx/components/GnomeList.jsx
+++ src/jsx/components/GnomeList.jsx
@@ -11,6 +11,29 @@ class GnomeList extends React.Component{
                 loaded: false, //if false display Spinner
             }
         }
+
+    loadData(limit){
+
+            const url = "http://master.datasource.jazzy-hr.jzapp.io/api/v1/gnomes?_format=json"
+            fetch(url+`&limit=${limit}&offset=0`)
+            .then(response => {
+                    if(response.ok) { 
+                        return response.json()
+                    } else {
+                        throw new Error("error");
+                    }
+            }).then( response => {
+                    this.setState({
+                        response,
+                        limit,
+                    })
+
+            }).catch( err => {
+                console.log("Błąd", err);
+            })
+        }
+
+    
 }
 
 module.exports = GnomeList;
