07b6b02fd95757be5917332ad6c9d5b0128d7a83
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-24 14:16:19 +0200

    config

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./components/Header.jsx";
+// import GnomeList from "components/GnomeList.jsx";
 
 document.addEventListener("DOMContentLoaded", function(){
+ 
+    class App extends React.Component{
+        render(){ 
+            return <div>    
+                   <Header />
+                   {/*<GnomeList />*/}
+                </div>                   
+        }           
 
-    console.log('test');
+    }
+
+    ReactDOM.render(
+    <App />, document.querySelector("#app")
+    )
 
 });
---
+++ src/jsx/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component{
+    render(){
+        console.log('header')
+        return  <header className="header">
+                TEST
+            </header>
+    }
+}
+
+module.exports = Header;
