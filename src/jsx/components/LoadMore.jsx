208e76f4b309ccc485b36ac40fad4001d7c942f0
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-24 15:25:30 +0200

    created LoadMore component - for loading next 100 gnomes from API

---
+++ src/jsx/components/LoadMore.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class LoadMore extends React.Component{
+    
+    render(){
+            return <button 
+                    className="button"
+                    onClick={this.handleLoadMore}>Load More</button>
+        }
+}
+
