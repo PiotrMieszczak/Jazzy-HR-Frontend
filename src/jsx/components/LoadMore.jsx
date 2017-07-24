b56fe8a2eefae41e36a6b372c7123df5772c953e
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-24 15:27:10 +0200

    updates with handle

--- src/jsx/components/LoadMore.jsx
+++ src/jsx/components/LoadMore.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 
 class LoadMore extends React.Component{
-    
+    handleLoadMore=()=>{
+        if(typeof this.props.moreGnomes ==='function'){
+                this.props.moreGnomes() //run getData fn with diffrent limit
+            }
+    }
     render(){
             return <button 
                     className="button"
