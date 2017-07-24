4aab7720fb622965d20b1bbedc1c864e1dbe9192
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-24 15:01:34 +0200

    added render elements

--- src/jsx/components/Gnome.jsx
+++ src/jsx/components/Gnome.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 class Gnome extends React.Component{
+    render(){
+            return <li key={this.props.gnome.id}
+                    className="gnomeList_gnome">
+                    <div
+                    className="gnomeList_gnome_logo"></div>
+                    <div
+                    className="gnomeList_gnome_name">{this.props.gnome.name}</div>
+                    <div
+                    className="gnomeList_gnome_age">{this.props.gnome.age}</div>
 
+                    <div 
+                    className="gnomeList_gnome_strenght">{this.props.gnome.strenght}+{'/100'}
+                    <span className="gnomeList_gnome_strenght_innerText">strength</span>
+                    </div>
+                </li>
+        }
 
 }
 
