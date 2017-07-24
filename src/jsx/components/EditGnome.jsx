15af93b4e40088a2c2fd14459946be184e07156d
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-24 17:16:36 +0200

    update form

--- src/jsx/components/EditGnome.jsx
+++ src/jsx/components/EditGnome.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
 class EditGnome extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            newName: '',
+            newAge: '',
+            newStr: '',
+        }
+    }
 
     render(){
         return <form>
             <label key={'gnomeNameLabel'} htmlFor="name">Gnome name:</label>
             <input 
+            value={this.state.newName}
+            defaultValue={this.props.gn.name}
             key={'gnomeName'} type="text" name="name"/>
             <label key={'gnomeAgeLabel'} htmlFor="age">Gnome name:</label>
             <input 
+            defaultValue={this.props.gn.age}
             key={'gnomeAge'} type="text" name="age"/>
             <label key={'gnomeStrLabel'} htmlFor="str">Strenght:</label>
             <input 
+            defaultValue={this.props.gn.strenght}
             key={'gnomeStr'} type="text" name="str"/>
             <input 
             type="submit" value="SAVE"/>
