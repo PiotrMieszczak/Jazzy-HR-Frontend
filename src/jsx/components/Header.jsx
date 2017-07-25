481914e12d7f1094928cb9079c17426d0d6fae22
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-25 12:41:20 +0200

    changed divs to ul list

--- src/jsx/components/Header.jsx
+++ src/jsx/components/Header.jsx
@@ -4,16 +4,18 @@ class Header extends React.Component{
     render(){
         return  <header>
                     <div className="container">
-                        <nav className="navbar">
-                        <div className="navbar_logo">logo</div>    
-                        <div className="navbar_monsterType">
-                            <div className="navbar_monsterType_innerText">gnomes</div>
-                            <div className="navbar_monsterType_innerText">trolls</div>
-                        </div>       
-                        <div className="header_buttonWrapper">
-                            <button className="button button--createMonster">create monster</button>
-                        </div>    
-                        <div className="header_avatar"></div>
+                        <nav>
+                        <ul className="navbar">
+                            <li className="navbar_logo">logo</li>    
+                            <li className="navbar_monsterType">
+                                <div className="navbar_monsterType_innerText">gnomes</div>
+                                <div className="navbar_monsterType_innerText">trolls</div>
+                            </li>       
+                            <li className="header_buttonWrapper">
+                                <div className="button button--createMonster">create monster</div>
+                            </li>    
+                            <li className="header_avatar"></li>
+                        </ul>
                     </nav>
                 </div> 
             </header>
