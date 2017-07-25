b5a5975fc52dd3e1245ae76e7eb82fdb0305ef45
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-25 12:38:29 +0200

    added container and nav tag

--- src/jsx/components/Header.jsx
+++ src/jsx/components/Header.jsx
@@ -2,16 +2,20 @@ import React from "react";
 
 class Header extends React.Component{
     render(){
-        return  <header className="navbar">
-                <div className="navbar_logo">logo</div>    
-                <div className="navbar_monsterType">
-                     <div className="navbar_monsterType_innerText">gnomes</div>
-                     <div className="navbar_monsterType_innerText">trolls</div>
-                </div>       
-                <div className="header_buttonWrapper">
-                       <button className="button button--createMonster">create monster</button>
-                </div>    
-                <div className="header_avatar"></div>    
+        return  <header>
+                    <div className="container">
+                        <nav className="navbar">
+                        <div className="navbar_logo">logo</div>    
+                        <div className="navbar_monsterType">
+                            <div className="navbar_monsterType_innerText">gnomes</div>
+                            <div className="navbar_monsterType_innerText">trolls</div>
+                        </div>       
+                        <div className="header_buttonWrapper">
+                            <button className="button button--createMonster">create monster</button>
+                        </div>    
+                        <div className="header_avatar"></div>
+                    </nav>
+                </div> 
             </header>
     }
 }
